96a4538c322d99f57460ad52d18106bcdf20bd98
LukashykYevhenii <lukashyk.yevhenii@chnu.edu.ua>
2024-11-14 17:42:28 +0200

    lesson10

--- lesson10/lesson10.py
+++ lesson10/lesson10.py
@@ -3,11 +3,26 @@ import sqlite3
 conn = sqlite3.connect('1222_DB.sl3', 5)
 cur = conn.cursor()
 # CREATE
+#cur.execute("CREATE TABLE student(id INT, name TEXT, age INT);")
 
-cur.execute("CREATE TABLE student(id int, name VARCHAR, age INT);")
+# INSERT
+# cur.execute("INSERT INTO student(id, name, age) VALUES (1,'Jack', 25)")
+# cur.execute("INSERT INTO student(id, name, age) VALUES (2,'Bob', 15)")
+# cur.execute("INSERT INTO student(id, name, age) VALUES (3,'Kevin', 12)")
+
+
+# SELECT
+cur.execute('SELECT * FROM student')
+conn.commit()
+
+res = cur.fetchall()
+print(res)
+
+conn.close()
+
 # ALTER
 # DROP
 
-# INSERT
-# SELECT
+
+
 
